034c761db2aa8de55c86dd1727b8b8a0b17f6415
Katrin Heiderer <katrin.heiderer@nagarro.com>
2022-06-02 17:27:55 +0200

    new branch for smartfridge java team

--- java/src/test/java/SmartFridgeApplication/SmartFridgeTest.java
+++ java/src/test/java/SmartFridgeApplication/SmartFridgeTest.java
@@ -1,4 +1,12 @@
 package SmartFridgeApplication;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SmartFridgeTest {
+
+    @Test
+    public void testFridgeCreationDate(){
+        Assert.assertTrue(false);
+    }
 }
