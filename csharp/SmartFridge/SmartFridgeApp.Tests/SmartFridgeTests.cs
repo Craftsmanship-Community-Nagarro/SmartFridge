5a3644d4ba2e663cce97d607acd7f93ffadb2f84
Alexander Schmid <Alexander.Schmid@anecon.com>
2022-06-02 17:33:45 +0200

    Add unittest project; change smartFridge project to class library

---
+++ csharp/SmartFridge/SmartFridgeApp.Tests/SmartFridgeTests.cs
@@ -0,0 +1,14 @@
+namespace SmartFridgeApp.Tests
+{
+    [TestClass]
+    public class SmartFridgeTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var smartFridge = new SmartFridge();
+            smartFridge.SomeMethod();
+
+        }
+    }
+}
