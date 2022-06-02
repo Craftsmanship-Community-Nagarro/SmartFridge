260acd14224310d03c88a634f9f44744b889d1eb
rgerm <roland.germ@admiral.at>
2022-06-02 18:02:50 +0200

    new failing test

--- csharp/SmartFridge/SmartFridgeApp.Tests/SmartFridgeTests.cs
+++ csharp/SmartFridge/SmartFridgeApp.Tests/SmartFridgeTests.cs
@@ -35,5 +35,26 @@ namespace SmartFridgeApp.Tests
             Assert.AreEqual(0, displayData.RemainingItems.Count);
         }
 
+        [TestMethod]
+        public void FridgeShouldContainNotExpiredMilk()
+        {
+            var smartFridge = new SmartFridge
+            {
+                CurrentDate = new DateTime(2022, 7, 15)
+            };
+
+            smartFridge.Add(
+                new Item
+                {
+                    Name = "Milk",
+                    Expiry = new DateTime(2022, 7, 15, 23, 59, 0)
+                });
+
+
+            var displayData = smartFridge.DisplayData();
+
+            Assert.AreEqual(0, displayData.ExpiredItems.Count);
+            Assert.AreEqual(1, displayData.RemainingItems.Count);
+        }
     }
 }
