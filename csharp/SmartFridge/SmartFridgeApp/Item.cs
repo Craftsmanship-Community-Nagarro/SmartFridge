e6489d78836bbe3578d6639c50e2aeb9bc850c93
rgerm <roland.germ@admiral.at>
2022-06-02 18:01:24 +0200

    added expired item

---
+++ csharp/SmartFridge/SmartFridgeApp/Item.cs
@@ -0,0 +1,9 @@
+namespace SmartFridgeApp;
+
+public class Item
+{
+    public string Name { get; set; }
+    public DateTime Added { get; set; }
+    public DateTime Expiry { get; set; }
+
+}
--- csharp/SmartFridge/SmartFridgeApp/SmartFridge.cs
+++ csharp/SmartFridge/SmartFridgeApp/SmartFridge.cs
@@ -8,13 +8,25 @@ namespace SmartFridgeApp
 {
     public class SmartFridge
     {
+        public DateTime CurrentDate { get; set; }
+        
+        private List<Item> items = new List<Item>();
+
         public DisplayData DisplayData()
         {
             return new DisplayData
             {
-                ExpiredItems = new List<string>(),
+                ExpiredItems = items.Select(item => item.Name).ToList(),
                 RemainingItems = new List<string>()
             };
         }
+
+        public void Add(Item item)
+        {
+            item.Added = CurrentDate;
+
+            items.Add(item);
+        }
+
     }
 }
